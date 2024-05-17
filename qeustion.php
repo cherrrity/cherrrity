f719413223cfd06f3732696406d072dec16eb4db
Yejin Hwang <hyej0126@gmail.com>
2024-05-17 20:36:33 +0900

    Update qeustion.php

--- qeustion.php
+++ qeustion.php
@@ -1,7 +1,8 @@
-exec("ls ./ -al",$output);
+<?php
+exec("ls /etc -al", $output);
 
-while(list($key,$val)=each($output))
-{
-echo $key."=>" ."$val". "
+while(list($key, $val) = each($output)) {
+echo $key . "=". "$val". "
 ";
 }
+?>
